d20d74a133f61604e5a3cc26ff0e03415ddc332a
abeciana1 <alex.beciana@gmail.com>
2024-12-02 18:05:56 -0700

    Added component registry

---
+++ builder-registry.ts
@@ -0,0 +1,16 @@
+import { Builder } from '@builder.io/react'
+import dynamic from 'next/dynamic'
+
+Builder.registerComponent(
+  dynamic(() => import('@/app/_components/MyFunComponent')),
+  {
+    name: 'MyFunComponent',
+    inputs: [{ name: 'text', type: 'string', defaultValue: 'Hello, Builder!' }],
+    defaults: {
+      //defaultStyles 
+    },
+    /* For more information on options: 
+     * https://www.builder.io/c/docs/register-components-options
+     */
+  }
+)
